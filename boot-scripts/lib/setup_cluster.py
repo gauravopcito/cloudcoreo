7364e67568be74606c67570e0aee115f5cfb905e
gauravopcito <gaurav.dabhade@opcito.com>
2016-09-05 00:31:26 +0530

    Updated cluster setup replica set configuration.

--- boot-scripts/lib/setup_cluster.py
+++ boot-scripts/lib/setup_cluster.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-from subprocess import call, check_output, Popen
+from subprocess import call, Popen, PIPE
 import yaml
 import sys
 
-DB_MOUNT_PATH = "/data/db/"
+MONGO_DATA_DIR = "/data/db/"
 AGENT_INSTALL_LOCATION = "/opt"
 MONGODB_PORT = "27017"
 CONFIG_SERVER_PORT = "27019"
@@ -20,38 +20,41 @@ MONGODB_LIMITS_CONF_FILE="/etc/security/limits.conf"
 MONGODB_NPROC_CONF_FILE="/etc/security/limits.d/90-nproc.conf"
 CLUSTER_FILE_LOCATION = "/etc/profile.d/cluster.yaml"
 is_master = False
+machine_ip = ""
 
 
-def write_cluster_file(ip_address_dicts):
+def write_cluster_file(ip_address_dict):
     '''
-    :param ip_address_dicts:
+    :param ip_address_dict:
     :return:
     '''
     cluster_file = open(CLUSTER_FILE_LOCATION, 'w+')
-    yaml.dump(ip_address_dicts, cluster_file, allow_unicode=False)
+    yaml.dump(ip_address_dict, cluster_file, allow_unicode=False)
 
     read_cluster_flile()
 
 
 def read_cluster_flile():
-    local_address = check_output("curl -sL 169.254.169.254/latest/meta-data/local-ipv4")
+    process = Popen(["curl", "-sL", "169.254.169.254/latest/meta-data/local-ipv4"], stdout=PIPE, stderr=PIPE)
+    curlstdout, curlstderr = process.communicate()
     stream = open(CLUSTER_FILE_LOCATION, "r")
     cluster_data = yaml.load_all(stream)
-    for data_list in cluster_data:
-        for data in data_list:
-            if data["private_ip_address"] == local_address:
-                is_master = data["is_master"]
+    for i, replica_item_list in enumerate(cluster_data.items()):
+        for replica_ips in replica_item_list[1]:
+            if replica_ips["private_ip"] == curlstdout:
+                is_master = replica_ips["is_master"]
+                machine_ip = replica_ips["private_ip"]
+    return cluster_data
 
 
 def configure_standalone_node():
     '''
+    configures the standalone node
     :return:
     '''
-    call("chkconfig mongod on")
-    call("service mongod start")
     call("ln -s /data/db/mongodb " + AGENT_INSTALL_LOCATION + "/mongodb")
 
-    command = AGENT_INSTALL_LOCATION + "/mongodb/bin/mongod  --port " + MONGODB_PORT + " --dbpath " + DB_MOUNT_PATH +\
+    command = AGENT_INSTALL_LOCATION + "/mongodb/bin/mongod  --port " + MONGODB_PORT + " --dbpath " + MONGO_DATA_DIR +\
           " < /dev/null > /dev/null 2>&1&  "
     call(command)
     call("echo \"" + command + "&\" >> /etc/rc.local")
@@ -66,16 +69,15 @@ def setup_cluster():
 
 def configure_replica_set():
     '''
+    Configures the replica set
     :return:
     '''
     # Configures three nodes replica set
     node_list = prepare_replica_nodes_list()
-    call("chkconfig mongod on")
-    call("service mongod start")
     call("ln -s /data/db/mongodb " + AGENT_INSTALL_LOCATION + "/mongodb")
 
-    command = AGENT_INSTALL_LOCATION + "/mongodb/bin/mongod --replSet " + "replicaSetName" + " --port " + MONGODB_PORT \
-              + " --logpath " + MONGO_DB_CONFIG_LOG_PATH + " --dbpath " + DB_MOUNT_PATH \
+    command = AGENT_INSTALL_LOCATION + "/mongodb/bin/mongod --replSet " + node_list[0] + " --port " + MONGODB_PORT \
+              + " --logpath " + MONGO_DB_CONFIG_LOG_PATH + " --dbpath " + MONGO_DATA_DIR \
               + " --rest < /dev/null > /dev/null 2>&1&  "
     call("echo \"" + command + "\" > /tmp/mongo.sh")
     call("bash /tmp/mongo.sh &")
@@ -91,56 +93,50 @@ def configure_replica_set():
     call("sed --in-place '$ i\\" + MONGODB_ULIMIT_VALUE3 + "' " + MONGODB_NPROC_CONF_FILE)
     call("sed --in-place '$ i\\" + MONGODB_ULIMIT_VALUE4 + "' " + MONGODB_NPROC_CONF_FILE)
 
-    # Update master node configuration
+    # if this is a master instance update node configuration
     if is_master:
         call("mongo")
         call("use admin")
-        process = Popen("rs.initiate({_id:replicaSetName, members: [{_id:1, host:" + node_list[1] + ":27017},{_id:1, host:"
-             + node_list[2] + ":27017}])")
-        (output, err) = process.communicate()
+        process = Popen("rs.initiate({_id:" + node_list[0] + ", members: [{_id:1, host:" + node_list[1][1]["private_ip"]
+                        + ":27017},{_id:2, host:" + node_list[1][2]["private_ip"] + ":27017}])")
+        process.communicate()
         process = Popen("rs.conf()")
-        (output, err) = process.communicate()
+        output, err = process.communicate()
 
 
 def prepare_replica_nodes_list():
     '''
-    :param:
-    :return:
+    return the list of nodes in replica
+    :return: node_list
     '''
-    local_address = check_output("curl -sL 169.254.169.254/latest/meta-data/local-ipv4")
-    stream = open(CLUSTER_FILE_LOCATION, "r")
-    cluster_data = yaml.load_all(stream)
+    cluster_data = read_cluster_flile()
+    for data in cluster_data:
+        for i,replica_item_list in enumerate(data.items()):
+            for replica_ips in replica_item_list[1]:
+                if replica_ips["private_ip"] == machine_ip:
+                    node_list = replica_item_list
+        break
 
-    mongo_node_list = []
-    for data_list in cluster_data:
-        for index, data in enumerate(data_list):
-            if data["private_ip_address"] == local_address:
-                mongo_node_list.append(data["private_ip_address"])
-                mongo_node_list.append(data_list[index+1]["private_ip_address"])
-                mongo_node_list.append(data_list[index+2]["private_ip_address"])
-                break
-
-    return mongo_node_list
+    return node_list
 
 
 instances = sys.argv[1]
 all_node_list = []
 nodes_dict = {}
-
-if len(instances.split()) > 1:
-    for i, instance in enumerate(instances):
-                if i == 10:
-            all_node_list.append({"private_ip_address": instance, "node_type": "Query", "is_master":"False"})
-        if i >= 6:
-            all_node_list.append({"private_ip_address": instance, "node_type": "Config", "is_master":"False"})
-        if i % 3 != 0:
-            all_node_list.append({"private_ip_address": instance, "node_type":"Secondary", "is_master":"False"})
+replica_sets = []
+instances = instances.split()
+if len(instances) > 1:
+    for index, instance in enumerate(instances):
+        if index % 3 != 0:
+            all_node_list.append({'private_ip': instance, "node_type": "primary", "is_master": True})
+            replica_sets.append(all_node_list)
         else:
-            all_node_list.append({"private_ip_address": instance, "node_type":"Primary", "is_master":"True"})
-        nodes_dict = all_node_list
+            all_node_list.append({'private_ip': instance, "node_type": "secondary", "is_master": False})
+            replica_sets.append(all_node_list)
+    nodes_dict = dict(replica_1=replica_sets)
 
-        write_cluster_file(nodes_dict)
-        setup_cluster()
+    write_cluster_file(nodes_dict)
+    setup_cluster()
 else:
     configure_standalone_node()
 
