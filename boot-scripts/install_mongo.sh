be68d7fc0f62342c736d867b23c251f5ca822178
gauravopcito <gaurav.dabhade@opcito.com>
2016-09-07 14:46:09 +0530

    solo_run

--- boot-scripts/install_mongo.sh
+++ boot-scripts/install_mongo.sh
@@ -3,7 +3,7 @@
 MONGO_DB_VERSION="${MONGO_DB_VERSION:-3.2.5}"
 MONGO_DATA_DIR="${MONGO_DATA_DIR:-}"
 
-sudo python -m pip install pymongo==3.1.1
+sudo python -m pip install pymongo==3.1.1  < /dev/null > /dev/null 2>&1&  
 
 echo "====================IN INSTALL MONGO SCRIPT=========================="
 yum install -y "mongodb-org-${MONGO_DB_VERSION}" "mongodb-org-server-${MONGO_DB_VERSION}" "mongodb-org-shell-${MONGO_DB_VERSION}" "mongodb-org-mongos-${MONGO_DB_VERSION}" "mongodb-org-tools-${MONGO_DB_VERSION}"
@@ -12,6 +12,8 @@ yum install -y "mongodb-org-${MONGO_DB_VERSION}" "mongodb-org-server-${MONGO_DB_
 if ! grep -q "exclude=mongodb-org,mongodb-org-server,mongodb-org-shell,mongodb-org-mongos,mongodb-org-tools" /etc/yum.conf; then
     echo "exclude=mongodb-org,mongodb-org-server,mongodb-org-shell,mongodb-org-mongos,mongodb-org-tools" >> /etc/yum.conf
 fi
+
+mkdir /data -p
 
 ## mongodb mounts in /data/db by default - if we want it somewhere else we keep that dir for simplicity and ln -sf it to the new path
 if [ -z "$MONGO_DATA_DIR" ]; then
