86345d705dd97e65d20b6841dabd8f4ef76d29f1
gauravopcito <gaurav.dabhade@opcito.com>
2016-09-08 16:13:54 +0530

    solo_run

--- boot-scripts/lib/setup_cluster.py
+++ boot-scripts/lib/setup_cluster.py
@@ -148,8 +148,8 @@ def add_database_user(node_list):
     :return:
     '''
     try:
-        call("/usr/bin/mongo " + node_list[1][0]["private_ip"] + ":" + MONGODB_PORT + "/" + "cloudcoreodb" + " --eval 'db.createUser( { user: \""
-             + "cloudcoreouser" + "\", pwd: \"" + "cloudcoreopass" + "\", roles: [ \"readWrite\" ] } )'", shell=True)
+        call("/usr/bin/mongo " + node_list[1][0]["private_ip"] + ":" + MONGODB_PORT + "/" + "cloudcoreodb" + " --eval 'printjson(db.createUser( { user: \""
+             + "cloudcoreouser" + "\", pwd: \"" + "cloudcoreopass" + "\", roles: [ \"readWrite\" ] } ))'", shell=True)
         print "User get created successfully."
     except Exception as e:
         print "User not get added."
