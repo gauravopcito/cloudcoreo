ccba04f6aaf1ffd4e9564314a290b4f30d73ef19
gauravopcito <gaurav.dabhade@opcito.com>
2016-09-12 17:45:32 +0530

    Added sharded cluster configuration.

--- boot-scripts/lib/setup_cluster.py
+++ boot-scripts/lib/setup_cluster.py
@@ -5,11 +5,12 @@ import yaml
 import sys
 import pymongo
 import time
-from pymongo import ReadPreference, collection, database, Connection
 import os
 
 MONGO_DATA_DIR = "/data/db/"
-AGENT_INSTALL_LOCATION = "/usr/bin/mongod"
+CONFIG_SERVER_DATA_DIR = "/data/configdb/"
+MONGOD_INSTALL_LOCATION = "/usr/bin/mongod"
+MONGOS_INSTALL_LOCATION = "/usr/bin/mongo "
 MONGODB_PORT = "27017"
 CONFIG_SERVER_PORT = "27019"
 QUERY_ROUTER_PORT = "27020"
@@ -17,8 +18,7 @@ YAML_PATH = "/etc/profile.d"
 MONGO_DB_CONFIG_LOG_PATH = "/var/log/mongodb/mongod.log"
 MONGO_DB_CONFIG_FILE_PATH="/etc/mongod.conf"
 CLUSTER_FILE_LOCATION = "/etc/profile.d/cluster.yaml"
-is_master = False
-machine_ip = ""
+MONGO_DB_CONFIG_SERVER_DATA_PATH = "/data/configdb"
 
 
 def write_cluster_file(ip_address_dict):
@@ -55,8 +55,9 @@ def get_machine_data():
             if replica_ips["private_ip"] == curlstdout:
                 is_master = replica_ips["is_master"]
                 machine_ip = replica_ips["private_ip"]
+                node_type = replica_ips["node_type"]
 
-    return is_master, machine_ip
+    return is_master, machine_ip, node_type
 
 
 def configure_standalone_node():
@@ -67,7 +68,7 @@ def configure_standalone_node():
     print "MongoDB standalone node configuration started..."
     call("service mongod stop", shell=True)
     call("mkdir " + MONGO_DATA_DIR + "  -p ", shell=True)
-    command = AGENT_INSTALL_LOCATION + " --port " + MONGODB_PORT + " --dbpath " + MONGO_DATA_DIR +\
+    command = MONGOD_INSTALL_LOCATION + " --port " + MONGODB_PORT + " --dbpath " + MONGO_DATA_DIR +\
           " < /dev/null > /dev/null 2>&1&  "
     call(command, shell=True)
     call("echo \"" + command + "&\" >> /etc/rc.local", shell=True)
@@ -78,33 +79,53 @@ def setup_cluster():
     '''
     :return:
     '''
-    configure_replica_set()
+    cluster_data = read_cluster_file()
+    is_master, machine_ip, node_type = get_machine_data()
+
+    for data in cluster_data:
+      for node_list in data.items():
+        for replica_ips in node_list[0][1]:
+            if replica_ips["private_ip"] == machine_ip:
+                if node_type == "primary" or node_type == "secondary":
+                    configure_replica_set(node_list, is_master)
+                    break
+                elif node_type == "config":
+                    config_server_host_list = data[len(data) - 2]["config"]
+                    configure_config_server(config_server_host_list)
+                    break
+                elif node_type == "router":
+                    query_routers_host_list = data[len(data)-1]["router"]
+                    config_server_host_list = data[len(data)-2]["config"]
+                    configure_query_routers(query_routers_host_list, config_server_host_list)
+                    add_shard_to_cluster(query_routers_host_list, node_list)
+                    add_database_and_shard_collections(query_routers_host_list)
+                    add_database_user(query_routers_host_list)
+                    break
 
 
-def configure_replica_set():
+def configure_replica_set(replica_host_list, config_server_list, is_master):
     '''
     Configures the replica set
     :return:
     '''
-    node_list = prepare_replica_nodes_list()
 
     print "Configure replica set of MongoDB started..."
     call("service mongod stop", shell=True)
     call("mkdir " + MONGO_DATA_DIR + "  -p ", shell=True)
-    command = AGENT_INSTALL_LOCATION + " --replSet " + node_list[0] + " --port " + MONGODB_PORT \
+    command = MONGOD_INSTALL_LOCATION + " --replSet " + replica_host_list[0] + " --port " + MONGODB_PORT \
               + " --logpath " + MONGO_DB_CONFIG_LOG_PATH + " --dbpath " + MONGO_DATA_DIR \
               + " --rest < /dev/null > /dev/null 2>&1&  "
     call("echo \"" + command + "\" > /tmp/mongo.sh", shell=True)
     call("bash /tmp/mongo.sh &", shell=True)
     call("echo \"" + command + "&\" >> /etc/rc.local", shell=True)
 
-    is_master, machine_ip = get_machine_data()
-
     # if this is a master instance update node configuration
     if is_master:
         connection = pymongo.MongoClient()
-        conf = {'_id': node_list[0],
-                     'members': [{'_id': 0, 'host': node_list[1][0]["private_ip"] + ":27017"}, {'_id': 1, 'host': node_list[1][1]["private_ip"] + ":27017"}, {'_id': 2, 'host': node_list[1][2]["private_ip"] + ":27017"}]}
+        conf = {'_id': replica_host_list[0],
+                     'members': [{'_id': 0, 'host': replica_host_list[1][0]["private_ip"] + ":27017"},
+                                 {'_id': 1, 'host': replica_host_list[1][1]["private_ip"] + ":27017"},
+                                 {'_id': 2, 'host': replica_host_list[1][2]["private_ip"] + ":27017"}]}
         db = connection.get_database("admin")
 
         retry = 1
@@ -127,38 +148,139 @@ def configure_replica_set():
                     print "Failed to configure replica set."
 
         add_collection()
-
         add_database_user()
 
 
-def add_collection():
+def configure_config_server(node_list):
+    '''
+    Configure config servers i.e mongo instances
+    :return:
+    '''
+    try:
+        print "Config server configuration started."
+        call("service mongod stop", shell=True)
+        call("mkdir " + CONFIG_SERVER_DATA_DIR + "  -p ", shell=True)
+        call("chown " + CONFIG_SERVER_DATA_DIR, shell=True)
+        command = MONGOD_INSTALL_LOCATION + " --configsvr --dbpath " + MONGO_DB_CONFIG_SERVER_DATA_PATH + " --logpath " + \
+                  MONGO_DB_CONFIG_LOG_PATH + " --port " + CONFIG_SERVER_PORT + " < /dev/null > /dev/null 2>&1&"
+        call("echo \"" + command + "\" > /tmp/mongo.sh", shell=True)
+        call("bash /tmp/mongo.sh &", shell=True)
+    except Exception as e:
+        print "Config server configuration fail." + e.message
+
+def configure_query_routers(config_server_host_list):
+    '''
+    Configure query routers i.e mongos instances
+    :return:
+    '''
+    try:
+        print "Query router configuration started."
+        call("service mongod stop", shell=True)
+        config_server_hostname_string = ""
+        number_of_host = len(config_server_host_list.items()[0][1])
+        for index, host_dict in enumerate(config_server_host_list.items()[0][1]):
+            if index != number_of_host-1:
+                config_server_hostname_string = config_server_hostname_string + host_dict["private_ip"] + ":" + CONFIG_SERVER_PORT + ","
+            else:
+                config_server_hostname_string = config_server_hostname_string + host_dict["private_ip"] + ":" + CONFIG_SERVER_PORT
+
+        command = MONGOD_INSTALL_LOCATION + " --logpath " + MONGO_DB_CONFIG_LOG_PATH + " --configdb " \
+                  + config_server_hostname_string + " < /dev/null > /dev/null 2>&1&"
+        call("echo \"" + command + "\" > /tmp/mongo.sh", shell=True)
+        call("bash /tmp/mongo.sh &", shell=True)
+        print "Query router configuration completed."
+    except Exception as e:
+        print "Query router configuration fail." + e.message
+
+
+def add_shard_to_cluster(query_routers_host_list, node_list):
+    '''
+    add shards to cluster
+    :return:
+    '''
+    try:
+        print "Add shard to cluster started."
+        query_router_host = query_routers_host_list[0]["private_ip"]
+        call("service mongod stop", shell=True)
+        for replica_ips in node_list[1][0]:
+            if replica_ips["node_type"] == "primary":
+                print "=======" + replica_ips[0] + "========="
+                # Here we need to specify replicaset name and host name to add shard
+                call(MONGOS_INSTALL_LOCATION + query_router_host + ":" + QUERY_ROUTER_PORT +
+                     "/admin --eval 'db.runCommand( {addShard : \"" + replica_ips[0] + "/" + replica_ips["private_ip"]
+                     + ":" + MONGODB_PORT + "\"})'")
+        print "Add shard to cluster completed."
+    except Exception as e:
+        print "Add shard to cluster fail" + e.message
+
+
+def add_database_and_shard_collections(query_routers_host_list):
+    '''
+    add database and shard collections
+    :return:
+    '''
+    try:
+        print "Add database and shard collection started."
+        query_router_host = query_routers_host_list[0]["private_ip"]
+        call("service mongod stop", shell=True)
+        call(MONGOS_INSTALL_LOCATION + query_router_host + ":" + QUERY_ROUTER_PORT
+             + "/admin --eval 'printjson(db.runCommand( { enableSharding: \"" + os.environ.get("DATABASE_NAME") + "\" }))'", shell=True)
+        print "Sharding enabled successfully."
+
+        is_automatic_hash_on_id_enable = 1
+        is_shard_keyhash_enable = 0
+        if is_automatic_hash_on_id_enable == 1:
+            print "In is_automatic_hash_on_id_enable."
+            call(MONGOS_INSTALL_LOCATION + query_router_host + ":" + QUERY_ROUTER_PORT + "/admin --eval 'printjson(db.runCommand( { shardCollection: \""
+                 + os.environ.get("DATABASE_NAME") + "." + os.environ.get("COLLECTION_NAME") + "\", key: { \"_id\": \"hashed\" }}))'", shell=True)
+            print "Sharded collection added successfully."
+        else:
+            # If no hash on shard key
+            if is_shard_keyhash_enable == 1:
+                print "In is_shard_keyhash_enable."
+                call(MONGOS_INSTALL_LOCATION + query_router_host + ":" + QUERY_ROUTER_PORT + "/admin --eval 'printjson(db.runCommand( { shardCollection: \""
+                     + os.environ.get("DATABASE_NAME") + "." + os.environ.get("COLLECTION_NAME") + "\", key: {\"" + "shardkey" + "\": \"hashed\"} } ))'", shell=True)
+                print "Out of is_shard_keyhash_enable."
+            else:
+                print "In else is_shard_keyhash_enable."
+                call(MONGOS_INSTALL_LOCATION + query_router_host + ":" + QUERY_ROUTER_PORT + "/" + os.environ.get("DATABASE_NAME")
+                     + " --eval 'printjson(db.createCollection(\"" + os.environ.get("COLLECTION_NAME") + "\"))'", shell=True)
+                call(MONGOS_INSTALL_LOCATION + query_router_host + ":" + QUERY_ROUTER_PORT + "/admin --eval 'printjson(db.runCommand( { shardCollection: \""
+                     + os.environ.get("DATABASE_NAME") + "." + os.environ.get("COLLECTION_NAME") + "\", key: {\"" + "shardkey" + "\":1} } ))'", shell=True)
+                print "Out of else is_shard_keyhash_enable."
+        print "Enable database and shard collection completed successfully."
+    except Exception as e:
+        print "Exception while enabling database and sharding collections." + e.message
+
+
+def add_collection(node_list):
     '''
     add collection
     :return:
     '''
     try:
-         #call("/usr/bin/mongo " + node_list[1][0]["private_ip"] + ":" + MONGODB_PORT + "/" + os.environ.get#("DATABASE_NAME")
-              #+ " --eval 'printjson(db.createCollection(\"" - + os.environ.get("COLLECTION_NAME") + #"\"))'", shell=True)
-         connection = Connection()
-         db = connection[os.environ.get("DATABASE_NAME")]
-         collection = db[os.environ.get("COLLECTION_NAME")]
-         print "Collection get created successfully."
+         print "Add collection started..."
+         call("/usr/bin/mongo " + node_list[1][0]["private_ip"] + ":" + MONGODB_PORT + "/" + os.environ.get("DATABASE_NAME")
+              + " --eval 'printjson(db.createCollection(\"" - + os.environ.get("COLLECTION_NAME") + "\"))'", shell=True)
+         print "Add collection completed successfully."
     except Exception as e:
-        print "Collection not get added. ==>>" + e.message
+        print "Exception while adding collection." + e.message
 
 
-def add_database_user():
+def add_database_user(node_list):
     '''
     add database user
     :return:
     '''
     try:
+        print "Add database user started..."
         call("/usr/bin/mongo " + node_list[1][0]["private_ip"] + ":" + MONGODB_PORT + "/" + os.environ.get("DATABASE_NAME")
              + " --eval 'printjson(db.createUser( { user: \"" - + os.environ.get("MASTER_USER") + "\", pwd: \"" +
              os.environ.get("MASTER_PASSWORD") + "\", roles: [ \"readWrite\" ] } ))'", shell=True)
-        print "User get created successfully."
+        print "Add database user completed successfully."
     except Exception as e:
-        print "User not get added. ==>>" + e.message
+        print "Exception while adding database user." + e.message
+
 
 def prepare_replica_nodes_list():
     '''
@@ -181,22 +303,42 @@ def prepare_replica_nodes_list():
 
 
 instances = sys.argv[1]
-all_node_list = []
 nodes_dict = {}
+rep1_list = []
+rep2_list = []
+config_server_list = []
+router_list = []
+nodes_list = []
 instances = instances.split()
 if len(instances) > 1:
     for index, instance in enumerate(instances):
-        if index % 3 != 0:
-            all_node_list.append({'private_ip': instance, "node_type": "secondary", "is_master": False})
-        else:
-            all_node_list.append({'private_ip': instance, "node_type": "primary", "is_master": True})
-    nodes_dict = dict(rs0=all_node_list)
+        if index < 3:
+            if index % 3 != 0:
+                rep1_list.append({'private_ip': instance, "node_type": "secondary", "is_master": False})
+            else:
+                rep1_list.append({'private_ip': instance, "node_type": "primary", "is_master": True})
+            if index == 2:
+                nodes_list.append(dict(rs0=rep1_list))
+        elif 2 < index < 6:
+            if index % 3 != 0:
+                rep2_list.append({'private_ip': instance, "node_type": "secondary", "is_master": False})
+            else:
+                rep2_list.append({'private_ip': instance, "node_type": "primary", "is_master": True})
+            if index == 5:
+                nodes_list.append(dict(rs1=rep2_list))
+        elif 5 < index < 9:
+                config_server_list.append({'private_ip': instance, "node_type": "config", "is_master": False})
+                if index == 8:
+                    nodes_list.append(dict(config=config_server_list))
+        elif index > 8:
+                router_list.append({'private_ip': instance, "node_type": "router", "is_master": False})
+                nodes_list.append(dict(router=router_list))
 
-    write_cluster_file(nodes_dict)
+    write_cluster_file(nodes_list)
     setup_cluster()
 else:
     for index, instance in enumerate(instances):
-        all_node_list.append({'private_ip': instance, "node_type": "standalone", "is_master": False})
-    nodes_dict = dict(standalone=all_node_list)
-    write_cluster_file(nodes_dict)
+        rep1_list.append({'private_ip': instance, "node_type": "standalone", "is_master": False})
+        nodes_list = dict(standalone=rep1_list)
+    write_cluster_file(nodes_list)
     configure_standalone_node()
